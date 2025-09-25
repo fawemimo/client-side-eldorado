709e7edcf9ad143effdd7270c00a5d7afb57d7fc
only1-adams <muhammedadam4544@gmail.com>
2025-09-25 13:39:36 +0100

    ui

--- src/app/(external_payments)/payments/invoices/page.jsx
+++ src/app/(external_payments)/payments/invoices/page.jsx
@@ -1,13 +1,15 @@
 import InvoicePaymentScreen from "@/components/page-components/external_payments/invoices/InvoicePaymentScreen";
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 import WalletProvider from "@/components/providers/WalletProvider";
 
 export default function page() {
 	return (
 		<WalletProvider>
 			<main className="flex flex-col lg:flex-row min-h-screen pt-[100px] lg:pt-0">
-				<InvoicePaymentScreen />
+				<Suspense>
+					<InvoicePaymentScreen />
+				</Suspense>
 			</main>
 		</WalletProvider>
 	);
--- src/app/(external_payments)/payments/payment-links/page.jsx
+++ src/app/(external_payments)/payments/payment-links/page.jsx
@@ -1,13 +1,15 @@
 import PaymentLinkScreen from "@/components/page-components/external_payments/invoices/payment-links/PaymentLinkScreen";
 import WalletProvider from "@/components/providers/WalletProvider";
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function page() {
 	return (
 		<WalletProvider>
 			<main className="flex flex-col lg:flex-row min-h-screen pt-[100px] lg:pt-0">
-				<PaymentLinkScreen />
+				<Suspense>
+					<PaymentLinkScreen />
+				</Suspense>
 			</main>
 		</WalletProvider>
 	);
